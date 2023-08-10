08759d36ed6723d101cf79a8a37c015b014e7c0e
Code-Gale <codewhiz001@gmail.com>
2023-08-10 17:03:23 +1300

    return of print

--- 0x00-python-hello_world/2-print.py
+++ 0x00-python-hello_world/2-print.py
@@ -1,6 +1,2 @@
 #!/usr/bin/python3
-<<<<<<< HEAD
 print("\"Programming is like building a multilingual puzzle\"")
-=======
-print('\"Programming is like building a multilingual puzzle\"')
->>>>>>> 5baba64dce565a27d0a632537993d6d447779b21
