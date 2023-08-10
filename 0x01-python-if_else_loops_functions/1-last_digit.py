c4abd596a96a48ecbe11eeb6e56c00e9bd189dc2
Code-Gale <codewhiz001@gmail.com>
2023-08-11 00:50:53 +0100

    the push of last digit

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -2,6 +2,8 @@
 import random
 number = random.randint(-10000, 10000)
 last = abs(number) % 10
+if (number < 0):
+    last = last * -1
 if (last < 6):
     print(f"Last digit of {number:d} is {last:} and is less than 6 and not 0")
 elif (last == 0):
