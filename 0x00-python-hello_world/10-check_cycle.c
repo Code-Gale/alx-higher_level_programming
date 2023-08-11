87fda7bec9da2c4e2ef2b013111cffe6aea73271
Code Gale <121695497+Code-Gale@users.noreply.github.com>
2023-08-11 03:19:19 +0100

    update cycle

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -1,38 +1,34 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
 #include "lists.h"
-
 /**
- * check_cycle - Check if a linked list contains a cycle
- * @list: a pointer to a linked list
+ * check_cycle - checks if a singly-linked list has a cycle.
+ * @list: linked list to be checked.
  *
- * Return: 0 (if no cycle), 1 (if there's cycle)
+ * Return: 0 if no cycle or 1 if there is a cycle.
  */
 int check_cycle(listint_t *list)
 {
-	listint_t *temp = NULL, *ptr = NULL;
-	int check = 0;
+	listint_t *slow = NULL;
+	listint_t *fast = NULL;
 
-	ptr = list;
-
-	if (ptr->next > list)
-		check = 1;
-	else
-		check = 0;
-
-	while (ptr != NULL)
+	/*Check if the list is null or contains only one node*/
+	if (list == NULL || list->next == NULL)
+		return (0);
+	slow = list;/*points to the first node*/
+	fast = list->next;/*points to second node*/
+	/*Traverse the list using slow and fast pointers*/
+	while (fast != NULL && fast->next)
 	{
-		temp = ptr;
-		ptr = ptr->next;
-		if (check == 1)
-		{
-			if (temp > ptr)
-				return (1);
-		}
-		else
-		{
-			if (temp < ptr)
-				return (1);
-		}
+		slow = slow->next;/*Move slow pointer by one step*/
+		fast = fast->next->next;/*Move fast pointer by two steps*/
+
+		/*If slow and fast pointers meet, there is a cycle in the list*/
+		if (slow == fast)
+			return (1);
 	}
 
+	/*If the loop completes without finding a cycle, return 0*/
 	return (0);
 }
