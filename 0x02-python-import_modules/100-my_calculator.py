754040f3cc8480c3596e96146a72f5cd18a34801
Code Gale <121695497+Code-Gale@users.noreply.github.com>
2023-08-11 13:37:31 +0100

    return of cal

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,23 +1,27 @@
 #!/usr/bin/python3
-from calculator_1 import add, sub, mul, div
-import sys
 
-if len(sys.argv) != 4:
-    print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-    sys.exit (1)
+if __name__ == "__main__":
+    import sys
 
-    a = int(arg[1])
-    operator = arg[2]
-    b = int(arg[3])
+    nargs = len(sys.argv) - 1
+    if nargs != 3:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        sys.exit(1)
 
-    if operator == '+':
-        result = add
-    elif operator == '-':
-        result = sub
-    elif operator == '*':
-        result = mul
-    elif operator == '/':
-        result = div
-    else:
+    op = sys.argv[2]
+    if op != '+' and op != '-' and op != '*' and op != '/':
         print("Unknown operator. Available operators: +, -, * and /")
-        sys.exit (1)
+        sys.exit(1)
+
+    from calculator_1 import add, sub, mul, div
+    a = int(sys.argv[1])
+    b = int(sys.argv[3])
+
+    if op == '+':
+        print("{} + {} = {}".format(a, b, add(a, b)))
+    elif op == '-':
+        print("{} - {} = {}".format(a, b, sub(a, b)))
+    elif op == '*':
+        print("{} * {} = {}".format(a, b, mul(a, b)))
+    else:
+        print("{} / {} = {}".format(a, b, div(a, b)))
