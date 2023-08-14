1303ff1347f46f24454dc29be277bba2ae0b6830
Code-Gale <codewhiz001@gmail.com>
2023-08-14 12:09:24 +0100

    updating  print_list

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-if __name__ == '__main__':
 
-    def print_list_integer(my_list=[]):
-        for ints in my_list:
-            print("{:d}".format(ints))
+def print_list_integer(my_list=[]):
+        for num in my_list:
+            print("{:d}".format(num))
