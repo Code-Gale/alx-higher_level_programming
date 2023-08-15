1b6ef390f8af968e71e0d090084398a9e60e40ae
Code-Gale <codewhiz001@gmail.com>
2023-08-15 16:01:06 +0100

    correction of replacing list

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-
 def print_reversed_list_integer(my_list=[]):
-    for rev in list(reversed(my_list)):
-        print("{:d}".format(rev))
+    if my_list:
+        for rev in list(reversed(my_list)):
+            print("{:d}".format(rev))
