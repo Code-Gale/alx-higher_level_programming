f6d34afdc1f3b7a512f74043d2d572b199a62de1
Code-Gale <codewhiz001@gmail.com>
2023-08-16 17:02:19 +0100

    push of square matrix

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,5 +1,3 @@
 #!/usr/bin/python
-def square_matrix_simple(matrix=[]):
-    if not matrix:
-        return
-    return[[num * num for num in i] for i in matrix]
+def square_matrix_simple(matrix=[]) -> list:
+    return [[j ** 2 for j in i] for i in matrix]
