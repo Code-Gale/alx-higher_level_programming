3baf71501a2e173fa7301a7ec0086aaba6b6512e
Code-Gale <codewhiz001@gmail.com>
2023-08-16 17:33:14 +0100

    fix of square matrix

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,3 +1,9 @@
 #!/usr/bin/python
-def square_matrix_simple(matrix=[]) -> list:
-    return [[j ** 2 for j in i] for i in matrix]
+def square_matrix_simple(matrix=[]):
+    updated_matrix = []
+    for row in matrix:
+        new_row = []
+        for value in row:
+            new_row.append(num ** 2)
+        new_matrix.append(new_row)
+    return new_matrix
