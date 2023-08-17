8cf40a890737485bd40d865fcbdf24d4c529bef4
Code-Gale <codewhiz001@gmail.com>
2023-08-17 22:43:34 +0100

    the ultimate return of square matrix map

--- 0x04-python-more_data_structures/101-square_matrix_map.py
+++ 0x04-python-more_data_structures/101-square_matrix_map.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
 def square_matrix_map(matrix=[]):
-    return list(map((lamda row: list(map((lambda x: x * x), row))), matrix))
+    return list(map((lamda x: list(map((lambda y: y**2, x))), matrix))
