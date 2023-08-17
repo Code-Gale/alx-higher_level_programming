fcd8de0851186b3db4a99aac6d17e83e317ad6ae
Code-Gale <codewhiz001@gmail.com>
2023-08-17 21:54:20 +0100

    the push of best score

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,7 @@
+#!/usr/bin/pytho3
+def best_score(a_dictionary):
+    if a_dictionary:
+        max_score = max(i for i in a_dictionary.values())
+        return "".join([x for x, y in a_dictionary.items() if y == max_score])
+    else:
+        return None
