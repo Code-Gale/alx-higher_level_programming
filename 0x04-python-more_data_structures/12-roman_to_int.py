820b9b7193a7b202d2abf771c2933e3472155264
Code-Gale <codewhiz001@gmail.com>
2023-08-17 22:24:35 +0100

    fix of roman to int

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -3,14 +3,14 @@ def roman_to_int(roman_string):
     if type(roman_string) is not str or roman_string is None:
         return 0
     roman_dic = {
-            'I' : 1,
-            'V' : 2,
-            'X' : 10,
-            'L' : 20,
-            'C' : 100,
-            'D' : 500,
-            'M' : 1000
-            }
+            'I': 1,
+            'V': 2,
+            'X': 10,
+            'L': 20,
+            'C': 100,
+            'D': 500,
+            'M': 1000
+    }
     decs = [roman_dic[x] for x in roman_string]
     output = 0
     for i in range(len(decs)):
