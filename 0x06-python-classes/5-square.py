c30afad5cfb422630008484b01d33128db4f3164
Code-Gale <codewhiz001@gmail.com>
2023-08-30 03:33:41 +0100

    push of my_print square

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""introducing my_print(self)"""
+
+
+class Square:
+    """def my_print(self)"""
+    def __init__(self, size=0):
+        self.size = size
+
+    @property
+    def size(self):
+        return size.__size
+
+    @size.setter
+    def size(self, value):
+        if type(value) is int:
+            if value < 0:
+                raise ValueError("size must be >= 0")
+            else:
+                self.__size = value
+        else:
+            raise TypeError("size must be an integer")
+
+    def area(self):
+        return int(self.__size) * int(self.__size)
+
+    def my_print(self):
+        if self.size == 0:
+            print()
+
+        for j in range(self.size):
+            for j in range(self.size):
+                print("#", end="")
+            print()
