f57d515a392d54b1dc07f8d6e56c03f4d84c8333
Code-Gale <codewhiz001@gmail.com>
2023-08-30 01:24:40 +0100

    fixing of empty square

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """creating an empty square class"""
+
+
 class Square:
-    '''Empty class named square with 
-    no fields and functions'''
+    '''Empty class named square with no fields and functions'''
     pass
