a126c454054a1a75d842a3ce60df27bec8918b5d
Code-Gale <codewhiz001@gmail.com>
2023-08-30 02:52:04 +0100

    push of getter and setter of square

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""introducing getter and setter"""
+
+
+class Square:
+    def __init__(self, size):
+
+        @property
+        def size(self):
+            return self.__size
+
+        @self.setter
+        def size(self, value):
+            if type(value) is int:
+                if value < 0:
+                    raise ValueError("size must be >= 0")
+                else:
+                    self.__size = value
+            else:
+                raise TypeError("size must be an integer")
+
+    def area(self):
+        return int(self.__size) * int(self.__size)
