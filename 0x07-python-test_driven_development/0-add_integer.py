13d36288e7766f9a88b523a2eae84d7ca5da83fe
Code-Gale <codewhiz001@gmail.com>
2023-09-04 13:41:05 +0100

    push of add integer and its test

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+This function adds 2 integers and returns
+the sum 
+"""
+
+def add_integer(a, b=98):
+    """
+    Calculates the sum of two ints
+    """
+    if not isinstance(a, (int, float)):
+        raise TypeError("a must be an integer")
+    if not isinstance(b, (int, float)):
+        raise TypeError("b must be an integer")
+    return int(a) + int(b)
---
+++ 0x07-python-test_driven_development/README.md
@@ -0,0 +1,1 @@
+Tasks for more Python Testing Driven Development
