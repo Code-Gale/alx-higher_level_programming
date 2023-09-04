528a95fade2cace9347a2956dd0dbaacf131e213
Code-Gale <codewhiz001@gmail.com>
2023-09-04 14:27:00 +0100

    push of square printing and its test

---
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+This function prints a sqaure
+"""
+
+
+def print_square(size):
+    """
+    Print a square based of the size passed to it
+    """
+    if not isinstance(size, (int, float)):
+        raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
+    if isinstance(size, float) and int(size) < 0:
+        raise TypeError("size must be an integer")
+    size = int(size)
+    for i in range(size):
+        print("#" * size)
