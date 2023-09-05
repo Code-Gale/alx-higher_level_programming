8fe1f02c2eae164e608fc347efb6ea0a331636c9
Code-Gale <codewhiz001@gmail.com>
2023-09-05 01:09:10 +0100

    push of rectangle with area and peri

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -14,7 +14,7 @@ class Rectangle:
     @property
     def width(self):
         return self.__width
-    
+
     @width.setter
     def width(self, value):
         if type(value) is not int:
@@ -39,6 +39,6 @@ class Rectangle:
         return self.__width * self.__height
 
     def perimeter(self):
-        if self.__width ==  or self.__height == 0:
+        if self.__width == 0 or self.__height == 0:
             return 0
         return (self.__width * 2) + (self.__height * 2)
