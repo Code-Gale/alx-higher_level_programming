85b18d54d3a5206263df22e4b83cf9e1ab7aaa8c
Code-Gale <codewhiz001@gmail.com>
2023-09-05 01:06:00 +0100

    push of rectangle with area and peri

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 """
-A rectangle class containing area
-and perimeter
+A rectangle class containing
+perimeter and area
 """
 
 
 class Rectangle:
-    """A class defining a rectangle and others"""
+    """ A rectangle class"""
     def __init__(self, width=0, height=0):
         self.width = width
         self.height = height
@@ -14,16 +14,15 @@ class Rectangle:
     @property
     def width(self):
         return self.__width
-
+    
     @width.setter
     def width(self, value):
         if type(value) is not int:
-            raise TypeError("width must be an integer")
+            raise TypeError("width must be an int")
         if value < 0:
             raise ValueError("width must be >= 0")
-        self.width = value
+        self.__width = value
 
-    """height property and property setter"""
     @property
     def height(self):
         return self.__height
@@ -31,15 +30,15 @@ class Rectangle:
     @height.setter
     def height(self, value):
         if type(value) is not int:
-            raise TypeError("height must be an integer")
+            raise TypeError("height must be an int")
         if value < 0:
             raise ValueError("height must be >= 0")
-        self.__height = value
+        self.height = value
 
     def area(self):
-        return self.__height * self.__width
+        return self.__width * self.__height
 
     def perimeter(self):
-        if self.__width == 0 or self.__height == 0:
+        if self.__width ==  or self.__height == 0:
             return 0
         return (self.__width * 2) + (self.__height * 2)
