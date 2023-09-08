79ab89f70ceaa50f0691eba013fdebc602441ca7
Code-Gale <codewhiz001@gmail.com>
2023-09-08 19:36:56 +0100

    fix of matrix mul

--- 0x07-python-test_driven_development/100-matrix_mul.py
+++ 0x07-python-test_driven_development/100-matrix_mul.py
@@ -8,9 +8,9 @@ def matrix_mul(m_a, m_b):
         m_a (list of lists): first matrix
         m_b (list of lists): second matrix
     """
-    if type(m_a) != list:
+    if type(m_a) is not list:
         raise TypeError("m_a must be a list")
-    if type(m_b) != list:
+    if type(m_b) is not list:
         raise TypeError("m_b must be a list")
 
     # variables to verify if both m_a and m_b can be multiplied
@@ -21,7 +21,7 @@ def matrix_mul(m_a, m_b):
     if m_a == []:
         raise ValueError("m_a can't be empty")
     for row1 in m_a:
-        if type(row1) != list:
+        if type(row1) is not list:
             raise TypeError("m_a must be a list of lists")
         len1 = len(m_a[0])
         if row1 == []:
@@ -30,42 +30,42 @@ def matrix_mul(m_a, m_b):
             raise TypeError("each row of m_a must be of the same size")
         num_colum1 = len(row1)
         for column1 in row1:
-            if type(column1) != int and type(column1) != float:
+            if type(column1) is not int and type(column1) is not float:
                 raise TypeError("m_a should contain only integers or floats")
 
     # Check requirements for matrix m_b
     if m_b == []:
         raise ValueError("m_b can't be empty")
     for row2 in m_b:
-        if type(row2) != list:
+        if type(row2) is not list:
             raise TypeError("m_b must be a list of lists")
         len2 = len(m_b[0])
         if row2 == []:
             raise ValueError("m_b can't be empty")
-        if len2 != len(row2):
+        if len2 is not len(row2):
             raise TypeError("each row of m_b must be of the same size")
         num_row2 += 1
         for column2 in row2:
-            if type(column2) != int and type(column2) != float:
+            if type(column2) is not int and type(column2) is not float:
                 raise TypeError("m_b should contain only integers or floats")
 
     # Check if the multiplication is posible
-    if num_colum1 != num_row2:
+    if num_colum1 ia not num_row2:
         raise ValueError("m_a and m_b can't be multiplied")
 
     mul_matrix = []
 
     for row_1 in m_a:
-        l = 0
-        l_row = []
-        while l < len(m_b[0]):
+        lop = 0
+        lop_row = []
+        while lop < len(m_b[0]):
             result = 0
             k = 0
             for column_1 in row_1:
                 result += column_1 * m_b[k][l]
                 k += 1
-            l_row.append(result)
-            l += 1
-        mul_matrix.append(l_row)
+            lop_row.append(result)
+            lop += 1
+        mul_matrix.append(lop_row)
 
     return mul_matrix
