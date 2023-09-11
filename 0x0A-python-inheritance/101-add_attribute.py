7158cefd5b28679335c495c6433b6f2caca80280
Code-Gale <codewhiz001@gmail.com>
2023-09-11 23:49:39 +0100

    adding attrib

---
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+Module ```101-add_attribute``` doc
+"""
+
+
+def add_attribute(a_class, name, value):
+    """ Set a new attribute if the attribute
+    does not exist
+    """
+    if not hasattr(a_class, "__dict__"):
+        raise TypeError("can't add new attribute")
+    setattr(a_class, name, value)
